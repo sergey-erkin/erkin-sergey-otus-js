2b5c768e57112deacf31b8e060a8812ffcd908f0
Sergey A Erkin <sergey.a.erkin@gmail.com>
2019-04-08 20:59:01 +0300

    #1 fix sum

--- javascript/javascript-1.js
+++ javascript/javascript-1.js
@@ -1,11 +1,11 @@
 function sum(addendum) {
   this.amount = this.amount || 0;
 
-  if (!addendum)
+  if (addendum === undefined || addendum === null)
     return this.amount;
 
   this.amount += Number(addendum);
   return sum;
 }
 
-console.log(sum(1)(2)(3)(4)(5)(6)());
+console.log(sum(1)(2)(3)(4)(5)(6)(0)(-1)());
